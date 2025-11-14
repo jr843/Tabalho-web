2497c2cf6c629109674e7ea120f96ed4edb2fdb6
junior <juniorvina2008@gmail.com>
2025-11-14 20:21:35 -0300

    Primeiro commit

---
+++ botaomenu.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botao = document.getElementById("botaoLogin");
+    botao.addEventListener("click", function () {
+        window.location.href = "loginmenu.html";
+    });
+});
